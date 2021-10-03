76500bb8d52520996f4889edc81d2cb96c5eebfb
DJ Taylor <dtaylor88@gmail.com>
2021-10-02 23:14:32 -0700

    remove authenticate before action temporarily

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,8 +4,6 @@ require 'json'
 class Api::V1::SessionsController < ApplicationController 
     include HTTParty
 
-    skip_before_action :authenticate
-
     def create 
       url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{params['_json']}"     
       response = HTTParty.get(url)
