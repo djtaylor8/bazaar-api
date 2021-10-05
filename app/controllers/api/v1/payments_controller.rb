d7badd67ccb97d02ff4be8717a28258cb6407191
DJ Taylor <dtaylor88@gmail.com>
2021-10-04 17:51:14 -0700

    set up params for payment

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -4,10 +4,18 @@ Stripe.api_key = ENV['stripe_secret']
 class Api::V1::PaymentsController < ApplicationController
 
     def create_payment
+        total_price = params[:amount].to_i
+        currency = params[:currency]
         payment_intent = Stripe::PaymentIntent.create(
-            amount: 1999,
-            currency: 'usd'
+            amount: total_price,
+            currency: currency
         )
         render json: { clientSecret: payment_intent.client_secret }
+    end
+
+    private 
+
+    def payment_params
+        params.permit(:amount, :currency, :paymentMethodType)
     end
 end
