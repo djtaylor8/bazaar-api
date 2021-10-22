a923308217033ad2d10c05b0cdc4b0364f7623e6
DJ Taylor <dtaylor88@gmail.com>
2021-10-22 14:34:29 -0700

    try new params in sessions

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::SessionsController < ApplicationController
     include HTTParty
 
     def create 
-      url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{session_params[:body]['_json']}" 
+      url = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{params['_json']}" 
       response = HTTParty.get(url)
       @user = User.create_user_for_google(response.parsed_response)
       @user.save
@@ -14,11 +14,5 @@ class Api::V1::SessionsController < ApplicationController
       else
         render json: { status: 'error', message: 'user could not be created' } 
       end 
-    end
-
-    private
-
-    def session_params
-      permit(:title, :body)
     end
 end
